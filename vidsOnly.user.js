52e09859cf8e19d20f06782da15117f3ea71f107
savedev <savedev@example.com>
2021-03-04 20:34:58 -0500

    beta

--- vidsOnly.user.js
+++ vidsOnly.user.js
@@ -1,20 +1,20 @@
 // ==UserScript==
 // @name         Vids Only
 // @namespace    https://github.com/VoltronicAcid/
-// @downloadURL  https://github.com/VoltronicAcid/
-// @version      0.0.1
+// @downloadURL  https://github.com/VoltronicAcid/vidsOnly/raw/main/vidsOnly.user.js
+// @version      0.1
 // @description  Set pages on https://sxyprn.com/ to display only videos
 // @author       VoltronicAcid
 // @match        https://sxyprn.com/blog/all/*
 // ==/UserScript==
 
 (() => {
-    const key = 'vidsOnly'
+    const vidsOnlyPreference = 'vidsOnly'
     const checkbox = document.querySelector('.vocontainer > input');
-    !checkbox.checked && (window.localStorage.getItem(key) === 'true') && checkbox.click();
-    window.localStorage.setItem(key, checkbox.checked);
+    !checkbox.checked && (window.localStorage.getItem(vidsOnlyPreference) === 'true') && checkbox.click();
+    window.localStorage.setItem(vidsOnlyPreference, checkbox.checked);
 
     checkbox.addEventListener('click', () => {
-        window.localStorage.setItem(key, checkbox.checked);
+        window.localStorage.setItem(vidsOnlyPreference, checkbox.checked);
     })
 })();
